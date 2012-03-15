126fe55c102feaf888e6b2f4c0b65a35c806ae14
nm <n.maisonneuve@gmail.com>
2012-03-15 17:07:19 +0100

    add  file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,14 +5,15 @@ Mechanicalmap::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.consider_all_requests_local       = true
+
+  config.action_controller.perform_caching = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
