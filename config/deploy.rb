bb953f7cfc0c494c58445c4061bf26d377a10f34
nm <n.maisonneuve@gmail.com>
2012-03-15 21:02:58 +0100

    test production

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ require "rvm/capistrano"
 set :rvm_ruby_string, '1.9.3'
 set :rvm_type, :user
 
-load 'deploy/assets'
+#load 'deploy/assets'
 
 
 # bundler bootstrap
@@ -48,11 +48,32 @@ ssh_options[:forward_agent] = true
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
+
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+ namespace :deploy do
+
+# cap deploy deploy:db_schema_load
+  desc "Load the initial schema - it will WIPE your database, use with care"
+  task :db_schema_load, :roles => :db, :only => { :primary => true } do
+    puts <<-EOF
+
+************************** WARNING ***************************
+If you type [yes], rake db:schema:load will WIPE your database
+any other input will cancel the operation.
+**************************************************************
+
+EOF
+    answer = Capistrano::CLI.ui.ask "Are you sure you want to WIPE your database?: "
+    if answer == 'yes'
+      run "cd #{current_path} && RAILS_ENV=production bundle exec rake db:schema:load"
+    else
+      puts "Cancelled."
+    end
+  end
+
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
