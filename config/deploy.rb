e2c0a56536b7003a17ab39074a1f00a802499739
nm <n.maisonneuve@gmail.com>
2012-03-15 16:09:51 +0100

    remove file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,22 @@
+set :default_environment, {
+  'PATH' => "/home/newhouse/.rvm/gems/ruby-1.9.3-p125/bin:/home/newhouse/.rvm/bin:$PATH",
+  'RUBY_VERSION' => 'ruby 1.9.3',
+  'GEM_HOME'     => '/home/newhouse/.rvm/gems/ruby-1.9.3-p125',
+  'GEM_PATH'     => '/home/newhouse/.rvm/gems/ruby-1.9.3-p125/',
+  'BUNDLE_PATH'  => '/home/newhouse/.rvm/gems/ruby-1.9.3-p125/'  # If you are using bundler.
+}
+require "rvm/capistrano"
+
+set :rvm_ruby_string, '1.9.3'
+set :rvm_type, :user
+
+load 'deploy/assets'
+
+
+# bundler bootstrap
 require 'bundler/capistrano'
+
+set :bundle_flags, ""
 
 
 set :user, 'newhouse'
@@ -7,10 +25,10 @@ set :application, "mechanicalmap"
 set :applicationdir, "~/mechanicalmap"
 
 
-set :repository, "git@github.com:vanpelt/rails-app.git"  # Your clone URL
+set :repository, "git://github.com/nmaisonneuve/mechanicalmap.git"  # Your clone URL
 set :scm, "git"
 set :scm_verbose, true
-
+set :git_enable_submodules, 1
 
 # roles (servers)
 role :web, domain
