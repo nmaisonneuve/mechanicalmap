37e43a2d975c08722feae3279748d76f1eaf75a1
nmaisonneuve <n.maisonneuve@gmail.com>
2012-05-21 12:17:18 +0200

    enhancing editor

--- app/assets/javascripts/abstract_micro_task.js
+++ app/assets/javascripts/abstract_micro_task.js
@@ -1,4 +1,4 @@
- /* ============================================================
+/* ============================================================
  * Abstract MicroTask class
  * NM
  * http://www.apache.org/licenses/LICENSE-2.0
@@ -6,12 +6,12 @@
 
 var AbstractMicroTask = Class.extend({
 
-    init:function(options) {
+    init:function (options) {
 
         this.scheduler_url = options.scheduler_url;
         this.user = options.user;
 
-        this.options=options;
+        this.options = options;
 
         // current task
         this.task = null;
@@ -19,6 +19,8 @@ var AbstractMicroTask = Class.extend({
         //project completeness
         this.task_completed = 0;
         this.task_done = 0;
+
+        this.cache=[];
 
         //HTML element id
         this.el_ids = {
@@ -30,72 +32,101 @@ var AbstractMicroTask = Class.extend({
             "task_answer":"task_answer",
             "form_task":"form_task"
         };
+        this.initialize_ui();
     },
 
-    update_completeness_ui:function() {
+    update_completeness_ui:function () {
         $("#" + this.el_ids["task_done"]).html(this.task_done);
         $("#" + this.el_ids["task_completed"]).html(this.task_completed);
         $("#" + this.el_ids["progress_bar_id"]).css.width((this.task_done / this.task_completed) * 100 + "%");
     },
-    initialize_ui:function() {
+    loading:function(){
+        console.log("loading");
+    },
+
+    initialize_ui:function () {
 
         var me = this;
+        $("#task_user_id").html(user.name);
         //bind ajax request to function load/save
-        $("#" + this.el_ids["skip_task"]).click(function(event) {
+        $("#" + this.el_ids["skip_task"]).click(function (event) {
             event.preventDefault();
-            me.request_task(me.task.id);
+            //me.request_task(me.task.id);
+            me.request_task_from_cache(me.task.id,me.load);
         });
 
         $("#" + this.el_ids["form_task"]).bind('ajax:before',
-            function() {
+            function () {
                 me.save();
             }).bind('ajax:success',
-            function(evt, data, status, xhr) {
-                me.request_task(me.task.id);
-            }).bind('ajax:error', function(data, status, xhr) {
+            function (evt, data, status, xhr) {
+                //me.request_task(me.task.id);
+                me.request_task_from_cache(me.task.id,me.load);
+            }).bind('ajax:error', function (data, status, xhr) {
                 console.log(data);
             });
     },
 
-    caching:function(current_task){
-        var from_task=current_task;
-        this.request_task(current_task);
-    },
 
-    request_task:function(from_task) {
-        // debug mode
-        if (this.options.debug_mode== true){
-            this.options.debug_request_task(from_task);
-        }else {
+    request_task_from_cache:function (from_task, callback) {
+        consumed = false;
+        var me=this;
+        console.log("caching data 1");
 
-        var me = this;
-        var query = (from_task == undefined) ? ".js" : ".js?from_task=" + from_task;
-          console.log("pre info loaded."+this.scheduler_url+""+query);
-        $.getJSON(this.scheduler_url + query,
-            function(task) {
-
-                if (task.submit_url) {
-                    me.load(task);
-                } else
-                    me.no_available_task();
-            })
-            .error(function(data, status, xhr) {
-                if (data.status == 404) {
-                    me.no_available_task();  // no task available
-                } else {
-                    console.log(data);
-                }
-            })
+        if (me.cache.length < 2) {
+            this.request_task(from_task, function (data) {
+                me.cache.push(data);
+                console.log("caching data");
+                if (!consumed) callback(me.cache.pop());
+                me.request_task_from_cache(data.task.id);
+            });
+        }
+        if ((me.cache.length > 0) && (callback !== null)) {
+            consumed = true;
+            callback(me.cache.pop());
         }
     },
 
-    load:function(unit) {
-        this.task = unit.task;
-        console.log("loading "+unit.submit_url);
-        $("#" + this.el_ids["form_task"]).attr("action", unit.submit_url);
+    request_task:function (from_task, success_callback) {
+        //default value
+        if (success_callback == null) {
+            success_callback = me.load
+        }
+
+        // debug mode
+        if (this.options.debug_mode == true) {
+            this.options.debug_request_task(from_task);
+        } else {
+
+            var me = this;
+            var query = (from_task == undefined) ? ".js" : ".js?from_task=" + from_task;
+            console.log("pre info loaded." + this.scheduler_url + "" + query);
+            $.getJSON(this.scheduler_url + query,
+                function (task) {
+
+                    if (task.submit_url) {
+
+                        success_callback(task);
+                    } else
+                        me.no_available_task();
+                })
+                .error(function (data, status, xhr) {
+                    if (data.status == 404) {
+                        me.no_available_task();  // no task available
+                    } else {
+                        console.log(data);
+                    }
+                })
+        }
     },
 
-    no_available_task: function () {
+    load:function (data) {
+        this.task = data.task;
+        console.log("loading " + data.submit_url);
+        $("#" + this.el_ids["form_task"]).attr("action", data.submit_url);
+    },
+
+    no_available_task:function () {
         alert("no task available");
         //abstract function  to implement in the sub class
     }
--- app/assets/javascripts/ft_micro_task3.js
+++ app/assets/javascripts/ft_micro_task3.js
@@ -13,75 +13,79 @@
 
 var FTMicroTask = AbstractMicroTask.extend({
 
-    init: function(options) {
+    init:function (options) {
         this._super(options);
         this.ft_table = options.ft_table;
         this.columns = [];
-        this.save = options.save;
-        this.load_internal = options.load;
-        this.loading = options.loading || function(){};
-        this.initialize_ui();
-        this.task_done=0;
-        this.task_total=1;
+
+
+
+        this.task_done = 0;
+        this.task_total = 1;
+
     },
 
-    start:function() {
+    start:function () {
         var me = this;
         // once the structure of a task is known
         // we request a task
-        $("#task_user_id").html(user.name);
-        this.load_schema(function() {
-            me.request_task();
+        this.load_schema(function () {
+            me.request_task(null);
+            //me.request_task_from_cache(null,me.load);
         });
         this.load_completeness();
     },
 
-    ft_request:function(query, callback_fct) {
+    ft_request:function (query, callback_fct) {
         // Builds a Fusion Tables SQL query and hands the result to dataHandler()
         var queryUrlHead = 'http://www.google.com/fusiontables/api/query?sql=';
         var queryUrlTail = '&jsonCallback=?'; // ? could be a function name
         // write your SQL as normal, then encode it
         query = queryUrlHead + query + queryUrlTail;
         var queryurl = encodeURI(query);
-        $.get(queryurl, function(data) {
+        $.get(queryurl, function (data) {
             callback_fct(data);
         }, "jsonp");
     },
 
-    load:function(task) {
-        this._super(task);
-        this.task_done++;
-        this.update_progress_bar();
-        // request more info about the task to the google fusion table
-        // and interpret the result to display it
+    request_task:function (from_task, success_callback) {
         var me = this;
-        var query = "SELECT ROWID, " + this.columns.join(",") + " FROM " + this.ft_table + " WHERE task_id='" + this.task.input + "'";
-        this.loading();
-        this.ft_request(query, function (ft_data) {
-            if (ft_data.table.rows.length == 0) {
-                me.no_available_task();
-            } else {
-                me.load_internal(ft_data);
-            }
+
+        this._super(from_task, function (task) {
+
+            //this.task_done++;
+            //this.update_progress_bar();
+            // request more info about the task to the google fusion table
+            // and interpret the result to display it
+            var query = "SELECT ROWID, " + me.columns.join(",") + " FROM " + me.ft_table + " WHERE task_id='" + task.task.input + "'";
+            me.ft_request(query, function (ft_data) {
+                if (ft_data.table.rows.length == 0) {
+                    me.no_available_task();
+                } else {
+                    task.data=ft_data;
+                    success_callback(task);
+                }
+            });
         });
     },
-    update_progress_bar:function() {
+
+    update_progress_bar:function () {
         var ratio = (this.task_done / this.task_total) * 100;
         $("#progress_bar").width(ratio + "%");
     },
-    load_completeness:function() {
-        var me=this;
-        $.get(application_url + "/user_state.js", function(data) {
-            me.task_done= data.completed;
-            me.task_total=me.task_done+data.opened;
+    load_completeness:function () {
+        var me = this;
+        $.get(application_url + "/user_state.js", function (data) {
+            me.task_done = data.completed;
+            me.task_total = me.task_done + data.opened;
             me.task_done--; //loading the current task
             me.update_progress_bar();
         });
     },
-    load_schema:function(callback_fct) {
+    load_schema:function (callback_fct) {
         var me = this;
-        this.ft_request("DESCRIBE " + this.ft_table, function(data) {
-            $.each(data.table.rows, function(i, row) {
+        this.ft_request("DESCRIBE " + this.ft_table, function (data) {
+            $.each(data.table.rows, function (i, row) {
                 me.columns.push(row[1]);
             });
             callback_fct();
