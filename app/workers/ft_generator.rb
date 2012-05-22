ed67c87fce8b07f16f0723d06ca92e77fd3390c0
nmaisonneuve <n.maisonneuve@gmail.com>
2012-05-22 02:25:07 +0200

    sidekiq

--- app/workers/ft_generator.rb
+++ app/workers/ft_generator.rb
@@ -2,14 +2,14 @@ class FtGenerator
 
   include Sidekiq::Worker
 
-  def perform(schema)
-
-    self.output_ft=FtDao.instance.create_table("Answers of #{self.name}", schema)
-    self.save
+  def perform(app_id,schema, user_email)
+     app=App.find(add_id)
+     app.output_ft=FtDao.instance.create_table("Answers of #{app.name}", schema)
+     app.save
 
     # set permission exportable
-    FtDao.instance.set_exportable(self.output_ft)
-    FtDao.instance.change_ownership(self.output_ft, user_email)
+    FtDao.instance.set_exportable(app.output_ft)
+    FtDao.instance.change_ownership(app.output_ft, user_email)
   end
 
 
